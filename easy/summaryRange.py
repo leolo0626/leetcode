9d8d9620866db7b0ca89718fd2b4110b3a55620c
leolo0626 <46132312+leolo0626@users.noreply.github.com>
2021-07-24 18:01:36 +0800

    Create summaryRange.py

---
+++ easy/summaryRange.py
@@ -0,0 +1,25 @@
+#Two Pointer
+class Solution:
+    def summaryRanges(self, nums: List[int]) -> List[str]:
+        result = []
+        start = end = 0 
+        while end < len(nums):
+            while end + 1 < len(nums) and nums[end] + 1 == nums[end+1] :
+                end = end + 1
+            
+            if nums[start] != nums[end] : 
+                result.append(f"{str(nums[start])}->{str(nums[end])}")
+            else : 
+                result.append(str(nums[start]))
+            
+            end = end + 1 
+            start = end
+        return result
+            
+            
+
+                
+                
+        
+    
+        
